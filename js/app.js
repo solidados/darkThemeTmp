d474e0d2b3b4d7fbcddb5417cec230eed942ad20
solidados <geekbrains.pk@internet.ru>
2022-06-25 21:55:10 +0400

    specified delay value && pre-loader logic

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
 // Since the content on this site is downloadable, all scripts must be executed after all media elements are fully loaded.
 window.onload = function() {
-    Draggable.create('.gallary', {
-        bounds: 'body',
-        inertia: true
+    // Pre-loader logic set up:
+    setTimeout(function() {
+
+        // Here I specify the value of the delay when the content is loaded:
+        document.body.classList.add('loaded')
+
+        Draggable.create('.gallary', {
+            bounds: 'body',
+            inertia: true
+        })
     })
 }
