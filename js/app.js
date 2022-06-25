8e6fcc99f094d9d7f172d42144447a47cef8f268
solidados <geekbrains.pk@internet.ru>
2022-06-25 23:36:30 +0400

    some corrections in code

--- js/app.js
+++ js/app.js
@@ -2,11 +2,10 @@
 window.onload = function() {
     // Pre-loader logic set up:
     setTimeout(function() {
-
         // Here I specify the value of the delay when the content is loaded:
         document.body.classList.add('loaded')
 
-        Draggable.create('.gallary', {
+        Draggable.create('.gallery', {
             bounds: 'body',
             inertia: true
         })
